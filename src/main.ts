433bc085944399290600d7965181b56b3f61677a
AndresEGV <agomez.vg@gmail.com>
2025-03-19 11:23:09 -0300

    add console log hola mundo

--- src/main.ts
+++ src/main.ts
@@ -18,6 +18,7 @@ async function bootstrap() {
   app.useGlobalFilters(new RpcCustomExceptionFilter());
 
   await app.listen(envs.port);
+  console.log('hola mundo')
 
   logger.log(`Gateway running on port ${envs.port}`);
 }
